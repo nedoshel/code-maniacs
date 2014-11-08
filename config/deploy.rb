38c7e47893f4abd22ddeb75a0c3c5d67109f1fbe
Alexandr Muravkin <nedoshel@gmail.com>
2014-11-08 14:05:56 +0300

    capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,10 +29,11 @@ set :rvm_ruby_version, '2.1.1'      # Defaults to: 'default'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp vendor/bundle}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
